5b038003e1244cb992c05ab3afe94d6bf85e5c7b
karl robeck alferez <karlalferezfx@gmail.com>
2023-07-17 20:36:43 +0800

    add fixed scrolling navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,8 +4,8 @@ import { Container, Navbar,Nav } from 'react-bootstrap'
 
 const Navigation = () => {
   return (
-    <Container className='position-relative'>
-        <Navbar className='d-flex justify-content-between py-3'>
+        <Navbar className='d-flex fixed-top justify-content-between py-3'>
+            <Container>
             <Navbar.Brand>
                 <img className='img-fluid' width={40} height={40} src="https://scontent.fmnl8-3.fna.fbcdn.net/v/t39.8562-6/120009688_325579128711709_1736249742330805861_n.png?_nc_cat=1&ccb=1-7&_nc_sid=6825c5&_nc_ohc=qvcMh0FhDYQAX_NanzB&_nc_ht=scontent.fmnl8-3.fna&oh=00_AfATe6dYfQpzwxysv5Fs_HcJmBr78fmcT0JVLYaciQJUSA&oe=64B5137D" alt="" />
             </Navbar.Brand>
@@ -31,8 +31,8 @@ const Navigation = () => {
                     </Nav.Link>
                 </Nav.Item>
             </Nav>
+            </Container>
         </Navbar>
-    </Container>
   )
 }
 
